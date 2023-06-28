e3315a66754ef8633f76a23b06bcc3c0bf872f57
Tirumalasetty Rayvalesh <rayvalesh@gmail.com>
2023-06-28 20:33:22 +0530

    Basic code for object detection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import records from './records.json';
+import styled from 'styled-components'
 
 function App() {
+  const objectDetectionData = records.map(item => item.inferenceanalytics.ObjectDetection);
+  const keys = []
+  const values = []
+  for (let eachobj of objectDetectionData) {
+    keys.push(Object.keys(eachobj));
+    values.push(Object.values(eachobj));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Object Detection Data:</h2>
+      <strong>Object</strong>
+      <strong>Count</strong>
+      {keys.map((key, index) => (
+        <div key={index}>
+           {key}
+           {values[index]}<br/>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
+
+
+const Box = styled.div`
+  height: 20px;
+  width: 20px;
+  color: red;
+`
