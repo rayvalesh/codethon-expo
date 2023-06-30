c6276e6d8ae5a99a5bc58fd53dd15e9504021787
Tirumalasetty Rayvalesh <rayvalesh@gmail.com>
2023-06-30 20:39:28 +0530

    data fetched

--- src/App.js
+++ src/App.js
@@ -4,34 +4,23 @@ import records from './records.json';
 import styled from 'styled-components'
 
 function App() {
-  const objectDetectionData = records.map(item => item.inferenceanalytics.ObjectDetection);
-  const keys = []
-  const values = []
-  for (let eachobj of objectDetectionData) {
-    keys.push(Object.keys(eachobj));
-    values.push(Object.values(eachobj));
+  const InferenceData = records.map(item => item.inferenceanalytics);
+  const filterdata = []
+  const listOfModels = ["ObjectDetection", "FaceAnalytics", "FaceRecognition", "AudioDetection"];
+  for (let inferobj of InferenceData) {
+    if (listOfModels.indexOf(Object.keys(inferobj)[0]) > -1) {
+      filterdata.push({ key: Object.keys(inferobj)[0], value: Object.values(inferobj)[0] });  
+    }
   }
+  console.log(filterdata)
+   
+
 
   return (
     <div>
       <h2>Object Detection Data:</h2>
-      <strong>Object</strong>
-      <strong>Count</strong>
-      {keys.map((key, index) => (
-        <div key={index}>
-           {key}
-           {values[index]}<br/>
-        </div>
-      ))}
     </div>
   );
 };
 
 export default App;
-
-
-const Box = styled.div`
-  height: 20px;
-  width: 20px;
-  color: red;
-`
